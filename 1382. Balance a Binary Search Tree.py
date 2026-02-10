eb2a924918f098820ed05e626921c446d44056cf
Saswasto Basak <89937629+saswasto@users.noreply.github.com>
2026-02-10 09:36:54 +0600

    Add files via upload

---
+++ 1382. Balance a Binary Search Tree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        nums = []
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            nums.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        def build(left, right):
+            if left > right:
+                return None
+            
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = build(left, mid - 1)
+            node.right = build(mid + 1, right)
+            return node
+
+        return build(0, len(nums) - 1)
---
+++ 3719. Longest Balanced Subarray I.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = 0
+
+        for i in range(n):
+            evens = set()
+            odds = set()
+
+            for j in range(i, n):
+                if nums[j] % 2 == 0:
+                    evens.add(nums[j])
+                else:
+                    odds.add(nums[j])
+
+                if len(evens) == len(odds):
+                    ans = max(ans, j - i + 1)
+
+        return ans
